0a2a90f6ae80a4762ea9e9d69e869089e5b4c181
AtanasovCode <a_ljupce@yahoo.com>
2024-04-01 17:08:44 +0200

    finished design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+
 const App = () => {
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState("");
+
+  const fetchAdvice = () => {
+    const url = 'https://api.adviceslip.com/advice?=' + Math.random(); // Appending a random query parameter
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.slip);
+        setAdvice(data.slip.advice);
+        setId(data.slip.id);
+      })
+      .catch(error => {
+        console.error('There was a problem fetching the advice:', error);
+      });
+  };
+
+
+  useEffect(() => {
+    fetchAdvice(); // Fetch advice on initial page load
+  }, []); // Empty dependency array to ensure this effect runs only once on mount
+
   return (
-    <div></div>
+    <div className="w-screen min-h-screen flex items-center justify-center bg-darkBlue font-manrope font-extrabold">
+      <Card advice={advice} id={id} fetchAdvice={fetchAdvice} />
+    </div>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import pauseIcon from '../assets/pattern-divider-mobile.svg'
+import dice from '../assets/icon-dice.svg';
+
+const Card = ({
+    fetchAdvice,
+    id, advice,
+}) => {
+    return (
+        <div className="
+            bg-darkGrayishBlue flex flex-col items-center justify-center w-[90%] md:w-[70%] lg:w-[30%] m-3 p-6 pb-16 relative rounded-xl text-lightCyan
+        ">
+            <div className="flex items-center justify-center text-neonGreen text-sm mb-4">
+                <div className="mr-2">Advice</div>
+                <div>#{id}</div>
+            </div>
+
+            <div className="text-center text-2xl mb-6">
+                {`"${advice}"`}
+            </div>
+
+            <div className="w-full flex items-center justify-center">
+                <img src={pauseIcon} alt="devider icon" />
+            </div>
+
+            <div className="
+                bg-neonGreen w-16 h-16 flex items-center justify-center rounded-full
+                absolute -bottom-7 left-50 -translate-x-50 cursor-pointer
+                hover:rotate-180 transition-all duration-500"
+                onClick={() => fetchAdvice()}
+            >
+                <img src={dice} alt="dice icon used to randomize advice" />
+            </div>
+        </div>
+    );
+}
+
+export default Card;
